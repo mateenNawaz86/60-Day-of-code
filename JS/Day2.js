c6e0cbeb72e18a70774216d78082d474c8702003
Mateen Mirani <mateenmirani125@gmail.com>
2022-08-06 21:49:54 +0500

    Finilize Webpage

---
+++ JS/Day2.js
@@ -0,0 +1,20 @@
+let scrolled = document.getElementById("scrolling");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrolled.style.display = "block";
+  } else {
+    scrolled.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
