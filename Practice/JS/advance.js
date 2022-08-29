bba522fc797ee402ee79bc600b3ea56f1ab96c26
Mateen Mirani <mateenmirani125@gmail.com>
2022-08-29 17:08:59 +0500

    Inheritance concepts

--- Practice/JS/advance.js
+++ Practice/JS/advance.js
@@ -138,3 +138,164 @@ const obj33 = {
 
 const obj44 = { ...obj1, ...obj33 };
 console.log(obj44);
+
+// Object litrals --> before ES6
+let fName = "Mateen";
+let lName = "Mirani";
+
+const objj1 = {
+  firstName: fName,
+  lastName: lName,
+};
+
+console.log(objj1);
+
+// ES6 --> using object litrals
+
+const objj2 = {
+  fName,
+  lName,
+};
+
+console.log(objj2);
+
+// Methods
+const objj3 = {
+  firstName: fName,
+  lastName: lName,
+  detail: function () {
+    return "My name is " + this.firstName + " " + this.lastName + ".";
+  },
+};
+
+console.log(objj3.detail());
+
+// Methods in ES6
+const obbj1 = {
+  firstName: fName,
+  lastName: lName,
+  detail() {
+    return `My name is ${this.firstName} ${this.lastName}.`;
+  },
+};
+
+// both return same results
+console.log(obbj1.detail());
+console.log(obbj1["detail"]());
+
+// Array Destructuring --> before ES6
+const arrDes = ["Mateen", 21, "Layyah"];
+
+console.log(arrDes[0]);
+console.log(arrDes[1]);
+console.log(arrDes[2]);
+
+// ES6
+const [namee, age, city] = arrDes;
+console.log(namee, age, city);
+
+const array_1 = [21, "Layyah", true, "Mateen"];
+let array_11 = array_1; // array Cloning
+console.log(array_11);
+
+let a, b, c;
+[a, b, c] = array_11;
+console.log(a, b, c);
+
+let new_arr = [21, "Layyah", true, ["Mateen", 34]];
+[a, b, c, [d, e]] = new_arr;
+console.log(d, e);
+
+// Object Destructuring
+
+let objDes = {
+  k: "My name",
+  l: 20,
+  m: "My city",
+};
+
+const objDes_1 = objDes; // Cloning
+console.log(objDes_1);
+
+let { k, l, m } = objDes;
+let { k: nameee, l: agee, m: cityy } = objDes;
+console.log(k, l, m);
+console.log(nameee);
+console.log(agee);
+console.log(cityy);
+
+// OOP concepts in ES6
+// 1. constructor method
+class Person {
+  constructor(name, passion) {
+    console.log("Constructor Method");
+    this.name = name;
+    this.passion = passion;
+  }
+}
+
+const const_1 = new Person("Mateen", "programming");
+console.log(const_1);
+
+// 2. Prototype method
+class ProtClass {
+  message() {
+    console.log("Hello, I have 4 bikes");
+  }
+}
+
+const protObj = new ProtClass();
+protObj.message();
+
+// 3. static method
+class StaticClass {
+  static msg() {
+    console.log("Hello, I'm from static class!");
+  }
+}
+
+StaticClass.msg();
+
+// Base class
+class Manager {
+  constructor(fullName, status, salary) {
+    this.mangName = fullName;
+    this.mangStatus = status;
+    this.mangSalary = salary;
+
+    // after obj creation constructor automatically calls
+    console.log("Constructor Method");
+  }
+
+  info() {
+    let travelAllownce = 6400;
+    let phoneAllownce = 1200;
+    let totalSalary = this.mangSalary + travelAllownce + phoneAllownce;
+
+    document.write(`<h3>Manager Class</h3> 
+    Manager Name is: ${this.mangName} <br>
+    Manager Status is: ${this.mangStatus} <br>
+    Manager Salary is: ${totalSalary}`);
+  }
+}
+
+// const managerObj = new Manager("Mateen");
+// managerObj.info();
+
+// Derived class
+class Employee extends Manager {
+  info() {
+    document.write(`<h3>Manager Class</h3> 
+    Manager Name is: ${this.mangName} <br>
+    Manager Status is: ${this.mangStatus} <br>
+    Manager Salary is: ${this.mangSalary}`);
+  }
+}
+
+const mangerObj = new Manager("Mateen", "Hr Manager", 76000);
+// Manager info()
+mangerObj.info();
+
+// Employee info()
+const empObj = new Employee("Mateen", "Hr Manager", 76000);
+empObj.info();
